d2adf3dd4028ec299bdc818e0b4e72da9112b13e
MehrnazVahedian <elyseeway@gmail.com>
2020-06-11 22:52:17 +0430

    My First Commit.

---
+++ app/src/main/java/com/example/film_yab/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.film_yab;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+public class MainActivity extends AppCompatActivity {
+ ListView mylistview;
+ String[] movies;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mylistview=findViewById(R.id.listview);
+        movies=new String[] {"me before you","great gatsby","blue jay","aqua man","wonder woman","me before you","great gatsby","blue jay","aqua man","wonder woman","x man","after"};
+   ArrayAdapter<String> myadapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,movies);
+   mylistview.setAdapter(myadapter);
+    }
+
+}
